8dc8004a83460ca26d36dff383192718807bcbc1
Kangwoosun <kws981024@gmail.com>
2017-11-21 15:32:06 +0900

    Final Project edit

--- OOP2_Final_Project_/Instructor.h
+++ OOP2_Final_Project_/Instructor.h
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <fstream>
+#include <vector>
 using namespace std;
 
 class Instructor {
@@ -17,4 +19,53 @@ public:
 	void SetInstructno(int);
 	void SetName(string);
 	void SetRank(string);
+	void StudentInformation(); //Instructor Main Menu Funtion
+	void InstructorInformation();//Instructor Main Menu Funtion
+	void QuestionInformation();//Instructor Main Menu Funtion
+	void SubjectInformation();//Instructor Main Menu Funtion
+	void GradeInformation();//Instructor Main Menu Funtion
+	char MainMenu();//Instructor Main Menu Interface
+	
 };
+
+void Instructor::StudentInformation() {
+	ifstream studentinfo("StudentList.bin", ios::in | ios::binary);
+}
+void Instructor::InstructorInformation() {
+	ifstream studentinfo("InstructorList.bin", ios::in | ios::binary);
+}
+void Instructor::QuestionInformation() {
+	ifstream studentinfo("QuestionList.bin", ios::in | ios::binary);
+}
+void Instructor::SubjectInformation() {
+	ifstream studentinfo("SubjectList.bin", ios::in|ios::binary);
+}
+void Instructor::GradeInformation() {
+
+}
+char Instructor::MainMenu() {
+	char output;
+	cout << "";
+	cin >> output;
+	return output;
+}
+
+int Instructor::GetInstructno() { return instructno; }
+string Instructor::GetName() { return name; }
+string Instructor::GetRank() { return rank; }
+void Instructor::SetInstructno(int a) {
+	instructno = a;
+}
+void Instructor::SetName(string a) {
+	name = a;
+}
+void Instructor::SetRank(string a) {
+	rank = a;
+}
+
+
+
+
+
+
+
--- OOP2_Final_Project_/OOP_Final_Project.cpp
+++ OOP2_Final_Project_/OOP_Final_Project.cpp
@@ -1,15 +1,90 @@
-#include <iostream>
+#include "Instructor.h"
+#include "Question.h"
+#include "Subject.h"
 #include <conio.h>
 using namespace std;
+
+
+char Login();
+Instructor Admin_IDsearch(string);
+Student Student_IDsearch(string);
+Instructor LoginAdmin;
+Stduent LoginAdmin;
 int main()
 {
-	char c = ' ';
+	char c;
+	while (c = Login() != 'x') {
+		switch (c)
+		{
+		case 's':
+
+			break;
+		case 'i':
+
+			break;
+		case 'q':
+
+			break;
+		case 'j':
+
+			break;
+		case 'g':
+
+			break;
+		case 'u':
+
+			break;
+		case 'b':
+
+			break;
+		default:
+
+		}
+
+	}
+	return 0;
+}
+
+char Login() {
+	ifstream Admin_Search("InstructorList.bin", ios::in | ios::binary);
+	ifstream Student_Search("StudentList.bin", ios::in | ios::binary);
+	char output;
+	static string instructor[4];
+	static string student[4];
 	string pass = "";
-	while (c != 13) {
-		c = getch();
-		if (c != 13) {
-			pass += c;
+	
+	for (int i = 0; i < 4; i++)
+	{
+		Admin_Search >> instructor[i];
+		Student_Search >> student[i];
+	}
+	//Input ID
+	while (!(cin >> pass)) {
+		if (instructor[1] == pass)
+			break;
+		else if (student[1] == pass)
+			break;
+		else 
+			for (int i = 0; i < 4; i++)
+			{
+				Admin_Search >> instructor[i];
+				Student_Search >> student[i];
+			}
+	}
+	
+
+
+	//Input PassWord
+	while (1) {
+		pass = "";
+		while(1) {
+			output = _getch();
+			pass += output;
 			cout << "*";
 		}
+	if (pass == "string")
+			break;
+		system("cls");
 	}
+	return output;
 }
